a206ed4e7a5c3950ee29cb79a7903a072aae8ae6
Berfongel <121492984+Berfongel@users.noreply.github.com>
2023-07-11 09:57:25 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,32 @@
+﻿using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QrCodeOlustur
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOlustur_Click(object sender, EventArgs e)
+        {
+            //Textbox a ne yazılıyorsa QR code okunduğunda yazılan yazı gelecek
+            string metin=tbMetin.Text;
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(metin, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(20);
+            pbQr.Image= qrCodeImage;
+        }
+    }
+}
